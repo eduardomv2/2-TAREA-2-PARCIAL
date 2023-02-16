f2113fc175c3a65749428efb221f205d192b4c69
eduar <eduar@DESKTOP-SV6GQVL>
2023-02-15 21:42:52 -0600

    Agregar archivos de proyecto.

---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub btnguardar_Click(sender As Object, e As EventArgs) Handles btnguardar.Click
+
+        Dim x As CONTACTO
+        x = New CONTACTO()
+
+        x.fechadenacimiento_ = DateTime.Parse("2003/01/01")
+
+        x.nombre_ = textNombre.Text
+        x.telefono_ = txtTelefono.Text
+
+        lblText.Text = x.ToString
+
+    End Sub
+
+
+End Class
---
+++ persona}.vb
@@ -0,0 +1,52 @@
+﻿Public Class persona_
+
+    Protected nombre As String
+    Protected fechadenacimiento As DateTime?
+
+
+
+    Public Property nombre_ As String
+        Get
+            Return nombre
+        End Get
+        Set(value As String)
+            nombre = value
+        End Set
+    End Property
+
+    Public Property fechadenacimiento_ As DateTime
+
+        Get
+            Return fechadenacimiento
+        End Get
+        Set(value As DateTime
+            )
+            fechadenacimiento = value
+        End Set
+    End Property
+    Public ReadOnly Property Edad() As String
+
+        Get
+            Dim edad_ As String
+            edad_ = (DateTime.Now.Year - fechadenacimiento.Value.Year)
+            Return edad_
+        End Get
+
+    End Property
+
+
+    Sub persona(nombre_, fechadenacimiento_)
+
+        nombre = nombre_
+        fechadenacimiento = fechadenacimiento_
+
+
+    End Sub
+
+    Public Overrides Function ToString() As String
+
+        Return nombre.ToUpper() + " " + Edad
+    End Function
+
+End Class
+
